4cc7b2d300b744eb443ca22f9f8285b8233f716c
KevinAK13 <kevin.guerra@uan.edu.com>
2025-03-12 02:07:48 -0600

    Fix dependencies and Webpack config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = { fs: false, path: false };
+    }
 
-const nextConfig: NextConfig = {
-  /* config options here */
+    // Cargar archivos .node correctamente
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: "node-loader",
+    });
+
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
